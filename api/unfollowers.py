c97c859ac5326eb1db6faceafad96a89a6eea589
Fahmi <fahmiardiansyah959@gmail.com>
2025-08-25 11:38:10 +0700

    Update unfollowers.py

--- api/unfollowers.py
+++ api/unfollowers.py
@@ -1,14 +1,18 @@
-# install instaloader: pip install instaloader
-import instaloader
+from instagrapi import Client
 import os
 
 def handler(request):
     username = os.environ.get("IG_USERNAME")
     password = os.environ.get("IG_PASSWORD")
-
-    L = instaloader.Instaloader()
+    
+    cl = Client()
     try:
-        L.login(username, password)
-        return {"status": "success", "message": "Login berhasil!"}
+        cl.login(username, password)
+        profile = cl.user_info_by_username(username)
+        return {
+            "status": "success",
+            "username": profile.username,
+            "full_name": profile.full_name
+        }
     except Exception as e:
         return {"status": "error", "message": str(e)}
