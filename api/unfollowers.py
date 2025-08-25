a70be99367f48a7f78b28a4e7343ba3bf1c5f0b2
Fahmi <fahmiardiansyah959@gmail.com>
2025-08-25 11:35:28 +0700

    Update unfollowers.py

--- api/unfollowers.py
+++ api/unfollowers.py
@@ -1,33 +1,14 @@
-# functions/unfollowers.py
+# install instaloader: pip install instaloader
+import instaloader
 import os
-import json
-from instagrapi import Client
 
-def handler(event, context):
-    USERNAME = os.environ.get("IG_USERNAME")
-    PASSWORD = os.environ.get("IG_PASSWORD")
-    TWO_FA = os.environ.get("IG_2FA")  # opsional
+def handler(request):
+    username = os.environ.get("IG_USERNAME")
+    password = os.environ.get("IG_PASSWORD")
 
-    client = Client()
-    client.delay_range = [0.5, 1]
-
+    L = instaloader.Instaloader()
     try:
-        if TWO_FA:
-            client.login(USERNAME, PASSWORD, verification_code=TWO_FA)
-        else:
-            client.login(USERNAME, PASSWORD)
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message": "Login berhasil",
-                "user_id": client.user_id
-            }),
-        }
+        L.login(username, password)
+        return {"status": "success", "message": "Login berhasil!"}
     except Exception as e:
-        return {
-            "statusCode": 401,
-            "body": json.dumps({
-                "error": f"Login gagal: {str(e)}"
-            }),
-        }
+        return {"status": "error", "message": str(e)}
