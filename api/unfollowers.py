a29ffea982a37b6bb4e7ea4f4f4857981078ebaf
Fahmi <fahmiardiansyah959@gmail.com>
2025-08-25 11:20:38 +0700

    Create unfollowers.py

---
+++ api/unfollowers.py
@@ -0,0 +1,31 @@
+import os
+
+from instagrapi import Client
+from dotenv import load_dotenv
+
+load_dotenv()  # lokal saja, di Vercel ENV pakai dashboard
+
+def handler(request):
+    USERNAME = os.environ.get("IG_USERNAME")
+    PASSWORD = os.environ.get("IG_PASSWORD")
+    TWO_FA = os.environ.get("IG_2FA")  # opsional
+@@ -17,14 +16,18 @@ def handler(request):
+            client.login(USERNAME, PASSWORD, verification_code=TWO_FA)
+        else:
+            client.login(USERNAME, PASSWORD)
+        # Login berhasil
+        return {
+            "statusCode": 200,
+            "body": {"message": "Login berhasil", "user_id": client.user_id}
+
+
+
+        }
+    except Exception as e:
+        # Login gagal
+        return {
+            "statusCode": 401,
+            "body": {"error": f"Login gagal: {str(e)}"}
+
+
+        }
